bf4a27872d5b345ba1c767e9d96c6cc1a8099d6c
Osama Ameer <osamaameer15@gmail.com>
2021-01-31 14:36:53 +0500

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,51 +1,37 @@
-let version = "v3";
+let cacheData = "PWA-Expense";
 
-//Cache Files
-let cacheFiles = [
-  "https://expense-tracker-pwa-cb63c.web.app/static/js/bundle.js",
-  "https://expense-tracker-pwa-cb63c.web.app/static/js/0.chunk.js",
-  "https://expense-tracker-pwa-cb63c.web.app/static/js/main.chunk.js",
-  "https://expense-tracker-pwa-cb63c.web.app/",
-  "https://expense-tracker-pwa-cb63c.web.app/index.html",
-  "https://fonts.googleapis.com/css?family=Lato&display=swap"
-];
-
-// Install Service Woker
-self.addEventListener("install", (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(version).then((cache) => {
-      console.log("cacheFiles", cacheFiles);
-      return cache.addAll(cacheFiles);
-    })
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(cacheData)
+      .then((cache) => {
+        cache.addAll([
+          "/",
+          "/index.html",
+          "/static/js/main.chunk.js",
+          "/static/js/0.chunk.js",
+          "/static/js/1.chunk.js",
+          "/static/js/bundle.js",
+          "/manifest.json",
+          "/favicon.ico",
+        ]);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      })
   );
 });
 
-// Activate Service Worker
-self.addEventListener("activate", function (e) {
-  console.log("[ServiceWorker] Activate");
-});
-
-const options = {
-  ignoreSearch: true,
-  ignoreMethod: true,
-  ignoreVary: true,
-};
-// Fetch Service Worker
-self.addEventListener("fetch", (event) => {
+this.addEventListener("fetch", (event) => {
   if (!navigator.onLine) {
     event.respondWith(
-      caches
-        .match(event.request, options)
-        .then((response) => {
-          if (response) {
-            // console.log(response);
-            return response || fetch.response;
-          }
-        })
-        .catch((err) => {
-          console.log("err", err);
-        })
+      caches.match(event.request).then((result) => {
+        if (result) {
+          return result;
+        }
+        let requestUrl = event.request.clone();
+        return fetch(requestUrl);
+      })
     );
   }
 });
--- src/swDev.js
+++ src/swDev.js
@@ -1,14 +1,6 @@
-//  REGISTRING THE SERVICE WORKER
-export function swDev(){
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-          navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-          });
-        });
-      }
- }
+export const swDev = () => {
+  let swDev = `${process.env.PUBLIC_URL}/sw.js`;
+  navigator.serviceWorker.register(swDev).then((result) => {
+    console.log(result, "result");
+  });
+};
